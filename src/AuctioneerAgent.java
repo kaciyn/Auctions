09f28649c7088d487b47d72cd6377f68fc298178
Kaci <25372015+kaciyn@users.noreply.github.com>
2020-10-23 19:37:27 +0200

    Working on AuctioneerAgent behaviours, in the middle of doing the AuctionServer (bid receiving step)

--- src/AuctioneerAgent.java
+++ src/AuctioneerAgent.java
@@ -36,7 +36,8 @@ public class AuctioneerAgent extends Agent
 //                done
     // The catalogue of items for sale (maps the item description to its price)
     private Hashtable<Integer, Item> catalogue;
-    private HashSet<AID> bidders;
+    private HashSet<AID> bidderAgents;
+    private int currentItemIndex;
 
     protected void setup()
     {
@@ -76,7 +77,7 @@ public class AuctioneerAgent extends Agent
             protected void handleElapsedTimeout()
             {
                 System.out.println("Starting auction");
-
+                currentItemIndex = 1;
                 addBehaviour(new AuctionServer());
             }
         });
@@ -96,7 +97,7 @@ public class AuctioneerAgent extends Agent
 // INFORM Message received. Process it
 
                 AID newBidderAID = msg.getSender();
-                bidders.add(newBidderAID);
+                bidderAgents.add(newBidderAID);
 
                 //leaving this for now
 
@@ -109,149 +110,163 @@ public class AuctioneerAgent extends Agent
 //                myAgent.send(reply);
             }
             else {
-                System.out.println("Unknown message type received");
+                System.out.println("Unknown/null message received");
                 block();
             }
 
         }
 
 
-        private class RequestPerformer extends Behaviour
+    }
+
+
+    private class AuctionItemServer extends CyclicBehaviour
+    {
+
+        public void action()
         {
-            private AID bestSeller; // The agent who provides the best offer
-            private int bestPrice; // The best offered price
-            private int repliesCnt = 0; // The counter of replies from seller agents private MessageTemplate mt; // The template to receive replies
-            private MessageTemplate mt; // The template to receive replies
-            private int step = 0;
+            var item = catalogue.get(currentItemIndex);
+            System.out.println("Auctioning item: " + item.Description);
 
-            public void action()
-            {
-                switch (step) {
-                    case 0:
-                        // Send the cfp to all sellers
-                        ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
-                        for (int i = 0; i < sellerAgents.length; ++i) {
-                            cfp.addReceiver(sellerAgents[i]);
-                        }
-                        cfp.setContent(targetBookTitle);
-                        cfp.setConversationId("book-trade");
-                        cfp.setReplyWith("cfp" + System.currentTimeMillis()); // Unique value myAgent.send(cfp);
-// Prepare the template to get proposals
-                        mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
-                                MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
-                        step = 1;
-                        break;
-                    case 1:
-                        // Receive all proposals/refusals from seller agents
-                        ACLMessage reply = myAgent.receive(mt);
-                        if (reply != null) {
-                            // Reply received
-                            if (reply.getPerformative() == ACLMessage.PROPOSE) { // This is an offer
-                                int price = Integer.parseInt(reply.getContent());
-                                if (bestSeller == null || price < bestPrice) {
-                                    // This is the best offer at present
-                                    bestPrice = price;
-                                    bestSeller = reply.getSender();
-                                }
-                            }
-                            repliesCnt++;
-                            if (repliesCnt >= sellerAgents.length) {
-                                // We received all replies
-                                step = 2;
-                            }
-                        }
-                        else {
-                            block();
-                        }
-                        break;
-                    case 2:
-                        // Send the purchase order to the seller that provided the best offer
-                        ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
-                        order.addReceiver(bestSeller);
-                        order.setContent(targetBookTitle);
-                        order.setConversationId("book-trade");
-                        order.setReplyWith("order" + System.currentTimeMillis());
-                        myAgent.send(order);
-                        // Prepare the template to get the purchase order reply
-                        mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"), MessageTemplate.MatchInReplyTo(order.getReplyWith()));
-                        step = 3;
-                        break;
-                    case 3:
-                        // Receive the purchase order reply
-                        reply = myAgent.receive(mt);
-                        if (reply != null) {
-                            // Purchase order reply received
-                            if (reply.getPerformative() == ACLMessage.INFORM) {
-// Purchase successful. We can terminate System.out.println(targetBookTitle+" successfully purchased."); System.out.println("Price = "+bestPrice);
-                                myAgent.doDelete();
-                            }
-                            step = 4;
-                        }
-                        else {
-                            block();
-                        }
-                        break;
-                }
-            }
+        }
 
-            public boolean done()
-            {
-                return ((step == 2 && bestSeller == null) || step == 4);
-            }
-        }  // End of inner class RequestPerformer
+        MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+        ACLMessage msg = myAgent.receive(mt);
+            if(msg !=null)
 
-
-        private class AuctionItemServer extends CyclicBehaviour
         {
-          for(
-            int i = 1; i<catalogue.size()+1;i++)
-
-            {
-                var item = catalogue.get(i);
-                System.out.println("Auctioning item: " + item.Description);
-
-            }
-
-
-            public void action()
-            {
-                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-                ACLMessage msg = myAgent.receive(mt);
-                if (msg != null) {
 // INFORM Message received. Process it
-                    String title = msg.getContent();
-                    ACLMessage reply = msg.createReply();
+            String title = msg.getContent();
+            ACLMessage reply = msg.createReply();
 
-                    Integer price = (Integer) catalogue.remove(title);
-                    if (price != null) {
-                        reply.setPerformative(ACLMessage.INFORM);
-                        System.out.println(title + " sold to agent " + msg.getSender().getName());
+            Integer price = (Integer) catalogue.remove(title);
+            if (price != null) {
+                reply.setPerformative(ACLMessage.INFORM);
+                System.out.println(title + " sold to agent " + msg.getSender().getName());
+            }
+            else {
+// The requested book has been sold to another buyer in the meanwhile .
+                reply.setPerformative(ACLMessage.FAILURE);
+                reply.setContent("not-available");
+            }
+            myAgent.send(reply);
+        }
+            else
+
+        {
+            block();
+        }
+    }
+
+
+    private class AuctionServer extends Behaviour
+    {
+        private AID highestBidder; // The agent who provides the best offer
+        private int highestBid; // The best offered price
+        private int responseCount = 0; // The counter of replies from seller agents private MessageTemplate mt; // The template to receive replies
+        private MessageTemplate mt; // The template to receive replies
+        private int step = 0;
+        Item item;
+
+        public void action()
+        {
+            switch (step) {
+                case 0:
+
+                    item = catalogue.get(currentItemIndex);
+                    System.out.println("Auctioning item: " + item.Description);
+
+                    // Send the cfp to all bidders
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+//iterate over bidder hashset
+                    for (AID bidder : bidderAgents) {
+                        cfp.addReceiver(bidder);
+                    }
+
+                    cfp.setContent(item.Description);
+                    cfp.setConversationId("auction-item");
+                    cfp.setReplyWith("cfp" + System.currentTimeMillis()); // Unique value myAgent.send(cfp);
+// Prepare the template to get proposals
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("bid-on-item"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step = 1;
+                    break;
+                case 1:
+                    // Receive all bids/refusals from bidder
+                    ACLMessage reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Reply received
+                        if (reply.getPerformative() == ACLMessage.PROPOSE) { // This is an offer
+                            int bid = Integer.parseInt(reply.getContent());
+                            if (highestBidder == null || bid > highestBid) {
+                                // This is the highest bid at present
+                                highestBid = bid;
+                                highestBidder = reply.getSender();
+                            }
+                        }
+                        responseCount++;
+                        if (responseCount >= bidderAgents.size()) {
+                            // received all responses
+                            if (highestBidder == null || highestBid < item.StartingPrice) {
+step=5;//nobody bid or price wasn't met
+                            }
+                            else {
+                                step = 2;
+
+                            }
+                        }
                     }
                     else {
-// The requested book has been sold to another buyer in the meanwhile .
-                        reply.setPerformative(ACLMessage.FAILURE);
-                        reply.setContent("not-available");
+                        block();
                     }
-                    myAgent.send(reply);
-                }
-                else {
-                    block();
-                }
+                    break;
+                case 2:
+                    // Send confirmation to bidder
+                    ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                    order.addReceiver(highestBidder);
+                    order.setContent(targetBookTitle);
+                    order.setConversationId("book-trade");
+                    order.setReplyWith("order" + System.currentTimeMillis());
+                    myAgent.send(order);
+                    // Prepare the template to get the purchase order reply
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"), MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+                    step = 3;
+                    break;
+                case 3:
+                    // Receive the purchase order reply
+                    reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Purchase order reply received
+                        if (reply.getPerformative() == ACLMessage.INFORM) {
+// Purchase successful. We can terminate System.out.println(targetBookTitle+" successfully purchased."); System.out.println("Price = "+bestPrice);
+                            myAgent.doDelete();
+                        }
+                        step = 4;
+                    }
+                    else {
+                        block();
+                    }
+                    break;
             }
-
-        } // End of inner class OfferRequestsServer
-
-        protected void takeDown()
-        {
-            // Deregister from the yellow pages
-            try {
-                DFService.deregister(this);
-            } catch (FIPAException fe) {
-                fe.printStackTrace();
-            }
-
-            System.out.println("Auctioneer " + getAID().getName() + " terminating.");
         }
+
+        public boolean done()
+        {
+            return ((step == 2 && highestBidder == null) || step == 4);
+        }
+    }  // End of inner class RequestPerformer
+
+
+    protected void takeDown()
+    {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        System.out.println("Auctioneer " + getAID().getName() + " terminating.");
     }
 
 
